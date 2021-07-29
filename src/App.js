18cbcc18bce349600702b40f4257aa904d57ac07
camjofindlay <camjofindlay@gmail.com>
2021-07-29 13:40:31 -0400

    push

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import Header from './components/Header';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+import Footer from './components/Footer'
+
+import porfolios from './portfolio'
+
+function App() {
+  return (
+<BrowserRouter>
+  <div class="container">
+    <Header />
+    <Switch>
+      <Route exact path={["/about", "/"]}>
+        <About />
+      </Route>
+      <Route exact path="/portfolio">
+        <Portfolio portfolios = {porfolios} />
+      </Route>
+      <Route path="/contact">
+        <Contact />
+      </Route>
+      <Route path="/resume">
+        <Resume />
+      </Route>
+    </Switch>
+    <Footer />
+  </div>
+</BrowserRouter>
+  )
+}
+
+export default App;
